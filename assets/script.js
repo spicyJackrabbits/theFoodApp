791978f8314bf797d5d232f41fb3ea269545aed1
Arash Saeedi <saeediarash84@gmail.com>
2021-02-01 22:53:23 -0500

    connected the zomato API updated some of the choices to work with the API more effieceintly

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,65 @@
+// get geolocation lat and long
+// 
+
+let lat = "";
+let lon = "";
+
+// asking permission to get user location
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(showPosition);
+    } else {
+        //   disable button?
+    }
+}
+
+function showPosition(position) {
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+}
+
+getLocation();
+
+
 $("#searchBtn").on("click", function () {
 
+    // user input data location, hunger level, and craving
+    console.log(lat);
+    console.log(lon);
 
-    var eatTime = $("#hungerLevel").find(":selected").text();
-    console.log(eatTime)
+    let hungerStage = $("#hungerLevel").find(":selected").text();
+    console.log(hungerStage)
 
-    var whatIWant = $("#cuisineOption").find(":selected").text();
-    console.log(whatIWant)
+    let theCrave = $("#cuisineOption").find(":selected").text();
+    console.log(theCrave)
 
-})
-    // let lat = ;
-    // let lon = ;
-    // let cuisine= ;
-//     var queryUrl= "https://developers.zomato.com/api/v2.1/search?entity_type=city&lat=33&lon=-84&radius=5&cuisines=asian&sort=real_distance;"
+    // connecting to the zomato API 
+    let cuisine = theCrave;
+
+    $.ajax({
+        method: "GET",
+        url: "https://developers.zomato.com/api/v2.1/search?lat=" + lat + "&lon=" + lon + "&q=" + cuisine + "&sort=real_distance",
+        headers: {
+            "user-key": "8d520ba5b6b4a6f2f35781a790c6fc0d",
+            "content-type": "application/json"
+        }
+    })
+        .then(response => {
+            console.log(response)
+            console.log(response.data.restaurants[0]);
+
+        })
+        // if (hungerStage === "Hangry") {
+        //     console.log("Oh you Hangry!!")
+        //     console.log(response.restaurants[0].name)
+            
+        //     // not sure this will bring the first one or not I kept requesting the API too much testing stuff out and cant test this
+        //     // i will test this tomorrow
+        //     // ###### we can have the link to the other page go here maybe and vice versa for else??
+        // } else {
+        //     console.log("Lets find a place to eat.")
+        // }
 
 
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function (response) {
-//           // Create a new table row element
-//     console.log(response)
-// })
+});
+
